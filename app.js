4f457b466c5828762748d62b5f1b0ebc238ea2de
Neha-Ujjwal <nehaujjwal1614@gmail.com>
2023-10-17 20:01:08 +0530

    Added Basic Layout

---
+++ app.js
@@ -0,0 +1,53 @@
+// const buttonElement = document.getElementById("btn");
+
+// function addCSS () {
+//     const textAreaElement = document.querySelector('textarea')
+//     const css = textAreaElement.value;
+//     const foreFront = document.querySelector('#forefront')
+//     foreFront.style.cssText=css;
+//     }
+
+
+// buttonElement.addEventListener('click', addCSS)
+
+
+
+
+
+// buttonElement.addEventListener('click', addCSS);
+const buttonElement = document.getElementById("btn");
+const textAreaElement = document.querySelector('textarea');
+
+function addCSS() {
+    const css = textAreaElement.value;
+    const foreFront = document.querySelector('#forefront');
+
+    // Regular expression to match flex properties
+    const flexPropertiesRegex = /^(?:flex-direction|flex-wrap|justify-content|align-items|align-content|order|flex-grow|flex-shrink|flex-basis|flex)\s*:\s*[^;]+(?:;|$)/gm;
+
+    // Check if the input contains anything other than flex properties
+    if (!flexPropertiesRegex.test(css)) {
+        // Apply error classes to the textarea for visual feedback
+        textAreaElement.classList.add('error-glow', 'error-shake');
+        setTimeout(() => {
+            textAreaElement.classList.remove('error-glow', 'error-shake');
+        }, 1000); // Remove error classes after 1 second
+        return;
+    }
+
+    // Check if the input contains any negative numbers
+    const negativeNumberRegex = /-[0-9]+/g;
+    if (negativeNumberRegex.test(css)) {
+        // Apply error classes to the textarea for visual feedback
+        textAreaElement.classList.add('error-glow', 'error-shake');
+        setTimeout(() => {
+            textAreaElement.classList.remove('error-glow', 'error-shake');
+        }, 1000); // Remove error classes after 1 second
+        return;
+    }
+
+    // Apply the CSS if it passes the checks
+    foreFront.style.cssText = css;
+}
+
+buttonElement.addEventListener('click', addCSS);
